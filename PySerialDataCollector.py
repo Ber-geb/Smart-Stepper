078624ebda73b0fd03108e2ae9875700ae1f57f3
Ber-geb <gebremed@unlv.nevada.edu>
2020-10-15 19:30:10 -0700

    Added Key Press Functionality & Fixed Exceptions
    - Now certain key presses will show the user's current action (i.e. walking = "w", running = "r', etc.)
    
    - Removed annoying exception that was thrown when ending data collection
    
    - Added error message when exception occurs for closing an unopened file

--- PySerialDataCollector.py
+++ PySerialDataCollector.py
@@ -10,6 +10,7 @@ from itertools import islice
 from tkinter import filedialog as fd
 from tkinter import ttk
 import csv
+import keyboard
 
 running = True
 f = ""
@@ -23,7 +24,52 @@ isFirst = True
 def timer(sp, interval):
     global f
     writer.writerow({'X': '-', 'Y': '-', 'Z': '-'})
+    temp = csv.DictWriter(datafile, fieldnames=labels, lineterminator='\n',
+                          delimiter='\n', quotechar='', quoting=csv.QUOTE_NONE, escapechar='\n')
     while running:
+        if (keyboard.is_pressed('w')):  # walking
+            keyboard.wait('esc')
+            print('w')
+            for i in range(10):
+                temp.writerow({'X': '', 'Y': '', 'Z': '\n'})
+            temp.writerow({'X': 'w', 'Y': '', 'Z': '\n'})
+        elif (keyboard.is_pressed('s')):  # standing
+            keyboard.wait('esc')
+            print('s')
+            for i in range(10):
+                temp.writerow({'X': '', 'Y': '', 'Z': '\n'})
+            temp.writerow({'X': 's', 'Y': '', 'Z': '\n'})
+        elif (keyboard.is_pressed('t')):  # toe-walking
+            keyboard.wait('esc')
+            print('t')
+            for i in range(10):
+                temp.writerow({'X': '', 'Y': '', 'Z': '\n'})
+            temp.writerow({'X': 't', 'Y': '', 'Z': '\n'})
+        elif (keyboard.is_pressed('r')):  # running
+            keyboard.wait('esc')
+            print('r')
+            for i in range(10):
+                temp.writerow({'X': '', 'Y': '', 'Z': '\n'})
+            temp.writerow({'X': 'r', 'Y': '', 'Z': '\n'})
+        elif (keyboard.is_pressed('a')):  # sitting
+            keyboard.wait('esc')
+            print('a')
+            for i in range(10):
+                temp.writerow({'X': '', 'Y': '', 'Z': '\n'})
+            temp.writerow({'X': 'a', 'Y': '', 'Z': '\n'})
+        elif (keyboard.is_pressed('l')):  # laying down flat
+            keyboard.wait('esc')
+            print('l')
+            for i in range(10):
+                temp.writerow({'X': '', 'Y': '', 'Z': '\n'})
+            temp.writerow({'X': 'l', 'Y': '', 'Z': '\n'})
+        elif (keyboard.is_pressed('j')):  # jumping
+            keyboard.wait('esc')
+            print('j')
+            for i in range(10):
+                temp.writerow({'X': '', 'Y': '', 'Z': '\n'})
+            temp.writerow({'X': 'j', 'Y': '', 'Z': '\n'})
+
         # sleep(interval)
         line = sp.readline().decode("utf-8")
         line = line.replace("\r", "").replace("\n", "")
@@ -36,7 +82,10 @@ def timer(sp, interval):
         # newRow = "%s,%s\n" % (t, line)
         # with open(f.name, "a") as datafile:
         # datafile.write(newRow)
-        writer.writerow(imuDict)
+        try:
+            writer.writerow(imuDict)
+        except Exception:
+            break
 
 
 def collect(strPort, interval):
@@ -66,8 +115,10 @@ def collect(strPort, interval):
 
     # writer.writeheader()
     sp = serial.Serial(strPort, 115200)
+
     time_thread = Thread(target=timer, args=(sp, interval))
     time_thread.start()
+    print(threading.activeCount())
 
 
 def qf(root):
@@ -90,13 +141,16 @@ def end():
     global running
     global datafile
 
-    datafile.close()
-    datafile = ""
-    tk.Label(root, text="Do you want to start another trial?").pack()
-    b = tk.Button(root, text="Yes", command=lambda: qf(root))
-    c = tk.Button(root, text="No", command=lambda: nf(root))
-    b.pack()
-    c.pack()
+    try:
+        datafile.close()
+        datafile = ""
+        tk.Label(root, text="Do you want to start another trial?").pack()
+        b = tk.Button(root, text="Yes", command=lambda: qf(root))
+        c = tk.Button(root, text="No", command=lambda: nf(root))
+        b.pack()
+        c.pack()
+    except Exception:
+        print("You are trying to close an unopened file")
 
 
 def onIncrement(counter):
@@ -128,12 +182,13 @@ def main():
     serial_menu.grid(row=0, column=1)
     counter = tk.IntVar()
     counter.set(1)
-    duration_label = ttk.Label(mainframe, textvariable=counter)
+    # duration_label = ttk.Label(mainframe, textvariable=counter)
+    duration_label = ttk.Label(mainframe, textvariable="")
     duration_label.grid(row=1, column=1)
-    duration_increment = ttk.Button(mainframe,
-                                    text="Increase Collection Interval (sec)",
-                                    command=lambda: onIncrement(counter))
-    duration_increment.grid(row=1, column=0)
+    # duration_increment = ttk.Button(mainframe,
+    #                                 text="Increase Collection Interval (sec)",
+    #                                 command=lambda: onIncrement(counter))
+    # duration_increment.grid(row=1, column=0)
     collect_button = ttk.Button(mainframe, text="Begin Data Collection",
                                 command=lambda: collect(serial_var.get(),
                                                         counter.get()))
